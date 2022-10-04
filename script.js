e13f23fb74bcbca44c724b981cf05540a04baa44
apple <wonderfulaya.2020@gmail.com>
2022-10-03 20:28:12 -0700

    縦線つけました。

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-$('.slider-1').slick({
-    arrows: false,
-    autoplay: true,
+$(function () {
+    $('.slider-1').slick({
+        arrows: false,
+        autoplay: true,
 
-});
+    });
 
+
+    // ここからが藤井です。
+
+    function ScrollTimelineAnime() {
+        $('.timeline li').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            var startPoint = 100;
+            if (scroll >= elemPos - windowHeight - startPoint) {
+                console.log("bingo");
+                var H = $(this).outerHeight(true)
+                var percent = (scroll + startPoint - elemPos) / (H / 2) * 100;
+                if (percent > 100) {
+                    percent = 100;
+                }
+                $(this).children('.border-line').css({
+                    height: percent + "%", //CSSでパーセント指定
+                });
+            }
+        });
+    }
+    $(window).on('scroll', function () {
+        ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
+    });
+    $(window).on('load', function () {
+        ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
+    });
+
+})
