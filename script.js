3a47f7c95fb555d2399b9be84f56ce909a817d1a
apple <wonderfulaya.2020@gmail.com>
2022-10-03 20:51:28 -0700

    Revert "変更"
    This reverts commit 6f730af173bb2abe25470b37bc3923d1dc3f14a8.

--- script.js
+++ script.js
@@ -17,14 +17,13 @@ $(function () {
             if (scroll >= elemPos - windowHeight - startPoint) {
                 console.log("bingo");
                 var H = $(this).outerHeight(true)
-                var percent = (scroll + startPoint- elemPos) / (H / 2) * 100;
+                var percent = (scroll + startPoint - elemPos) / (H / 2) * 100;
                 if (percent > 100) {
                     percent = 100;
                 }
                 $(this).children('.border-line').css({
                     height: percent + "%", //CSSでパーセント指定
                 });
-                console.log(percent);
             }
         });
     }
